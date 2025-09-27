7819efa9be6cd91edb6b52d26f753ad209d795dc
amanullahshah32 <nafi17211@gmail.com>
2025-09-27 23:07:03 +0600

    comparison operator practiced in js

--- script.js
+++ script.js
@@ -1,50 +1,83 @@
 let js = "amazing";
-if (js === "amazing")
-alert("javascript is fun");
+// if (js === "amazing")
+// alert("javascript is fun");
 
-//40 + 60 - 10;
-console.log(40 + 60 - 10);
+// //40 + 60 - 10;
+// console.log(40 + 60 - 10);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Jonas";
-console.log(firstName); 
-console.log(firstName); 
-console.log(firstName); 
+// let firstName = "Jonas";
+// console.log(firstName); 
+// console.log(firstName); 
+// console.log(firstName); 
 
-let Person = "Aman";
-let pi = 3.1416;
+// let Person = "Aman";
+// let pi = 3.1416;
 
-const PI= 3.1416; 
+// const PI= 3.1416; 
 
-console.log(Person);
-console.log(pi);
-console.log(PI);
+// console.log(Person);
+// console.log(pi);
+// console.log(PI);
 
-let myFirstJob = "Apex developer";
-let myCurrentJob = "Software developer";
+// let myFirstJob = "Apex developer";
+// let myCurrentJob = "Software developer";
 
-console.log(myFirstJob);
-console.log(myCurrentJob);
+// console.log(myFirstJob);
+// console.log(myCurrentJob);
 
-let javaScriptIsFun = true;
-console.log(javaScriptIsFun);
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
 
-console.log(typeof true);
-console.log(typeof myFirstJob);
-console.log(typeof javaScriptIsFun);
-console.log(typeof 'aman');
-console.log(typeof 345);
+// console.log(typeof true);
+// console.log(typeof myFirstJob);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 'aman');
+// console.log(typeof 345);
 
-javaScriptIsFun = 'Yes';
-console.log(typeof javaScriptIsFun);
+// javaScriptIsFun = 'Yes';
+// console.log(typeof javaScriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 20021;
-console.log(typeof year);
+// year = 20021;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+const now = 2050;
+console.log(now);
+
+const ageAman = now - 2001;
+const ageG = now - 1999;
+console.log(ageAman, ageG);
+
+console.log(ageAman * 2, ageAman / 10, 2** 3); // power funciton, multiplication and division
+
+const firstName = 'Amanullah';
+const lastName = "Shah";
+console.log('fullname is : ' + firstName + ' ' + lastName); 
+
+//assignment operators
+let x = 10 + 5; 
+console.log('value of x is; ' + x);
+
+x+= 10;
+
+console.log('value of x after += is; ' + x);
+
+x++;
+x--;
+x--;
+console.log("new value of x is; " + x);
+
+//comparison operators
+console.log(ageAman > ageG);
+console.log(ageG >= 18);
+
+const isFullAge = ageG >= 18; 
+console.log(isFullAge);
