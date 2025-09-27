e979b417c02697088b365464c387e848e4c4f134
amanullahshah32 <nafi17211@gmail.com>
2025-09-27 19:11:20 +0600

    learned variable types

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
+let js = "amazing";
+if (js === "amazing")
+alert("javascript is fun");
 
+//40 + 60 - 10;
+console.log(40 + 60 - 10);
 
-    let js = "amazing";
-    if (js === "amazing")
-    alert("javascript is fun");
+console.log("Jonas");
+console.log(23);
 
-    40 + 60 - 10;
-    console.log(40 + 60 - 10);
+let firstName = "Jonas";
+console.log(firstName); 
+console.log(firstName); 
+console.log(firstName); 
+
+let Person = "Aman";
+let pi = 3.1416;
+
+const PI= 3.1416; 
+
+console.log(Person);
+console.log(pi);
+console.log(PI);
+
+let myFirstJob = "Apex developer";
+let myCurrentJob = "Software developer";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
