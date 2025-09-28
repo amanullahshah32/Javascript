5bfe9c1f25000063cb39ab141949880c0aca9ca9
amanullahshah32 <nafi17211@gmail.com>
2025-09-28 12:12:50 +0600

    codding challenge 1

--- script.js
+++ script.js
@@ -1,95 +1,113 @@
-let js = "amazing";
-// if (js === "amazing")
-// alert("javascript is fun");
+// let js = "amazing";
+// // if (js === "amazing")
+// // alert("javascript is fun");
 
-// //40 + 60 - 10;
-// console.log(40 + 60 - 10);
+// // //40 + 60 - 10;
+// // console.log(40 + 60 - 10);
 
-// console.log("Jonas");
-// console.log(23);
+// // console.log("Jonas");
+// // console.log(23);
 
-// let firstName = "Jonas";
-// console.log(firstName); 
-// console.log(firstName); 
-// console.log(firstName); 
+// // let firstName = "Jonas";
+// // console.log(firstName); 
+// // console.log(firstName); 
+// // console.log(firstName); 
 
-// let Person = "Aman";
-// let pi = 3.1416;
+// // let Person = "Aman";
+// // let pi = 3.1416;
 
-// const PI= 3.1416; 
+// // const PI= 3.1416; 
 
-// console.log(Person);
-// console.log(pi);
-// console.log(PI);
+// // console.log(Person);
+// // console.log(pi);
+// // console.log(PI);
 
-// let myFirstJob = "Apex developer";
-// let myCurrentJob = "Software developer";
+// // let myFirstJob = "Apex developer";
+// // let myCurrentJob = "Software developer";
 
-// console.log(myFirstJob);
-// console.log(myCurrentJob);
+// // console.log(myFirstJob);
+// // console.log(myCurrentJob);
 
-// let javaScriptIsFun = true;
-// console.log(javaScriptIsFun);
+// // let javaScriptIsFun = true;
+// // console.log(javaScriptIsFun);
 
-// console.log(typeof true);
-// console.log(typeof myFirstJob);
-// console.log(typeof javaScriptIsFun);
-// console.log(typeof 'aman');
-// console.log(typeof 345);
+// // console.log(typeof true);
+// // console.log(typeof myFirstJob);
+// // console.log(typeof javaScriptIsFun);
+// // console.log(typeof 'aman');
+// // console.log(typeof 345);
 
-// javaScriptIsFun = 'Yes';
-// console.log(typeof javaScriptIsFun);
+// // javaScriptIsFun = 'Yes';
+// // console.log(typeof javaScriptIsFun);
 
-// let year;
-// console.log(year);
-// console.log(typeof year);
+// // let year;
+// // console.log(year);
+// // console.log(typeof year);
 
-// year = 20021;
-// console.log(typeof year);
+// // year = 20021;
+// // console.log(typeof year);
 
-// console.log(typeof null);
+// // console.log(typeof null);
 
-const now = 2050;
-console.log(now);
+// const now = 2050;
+// console.log(now);
 
-const ageAman = now - 2001;
-const ageG = now - 1999;
-console.log(ageAman, ageG);
+// const ageAman = now - 2001;
+// const ageG = now - 1999;
+// console.log(ageAman, ageG);
 
-console.log(ageAman * 2, ageAman / 10, 2** 3); // power funciton, multiplication and division
+// console.log(ageAman * 2, ageAman / 10, 2** 3); // power funciton, multiplication and division
 
-const firstName = 'Amanullah';
-const lastName = "Shah";
-console.log('fullname is : ' + firstName + ' ' + lastName); 
+// const firstName = 'Amanullah';
+// const lastName = "Shah";
+// console.log('fullname is : ' + firstName + ' ' + lastName); 
 
-//assignment operators
-let x = 10 + 5; 
-console.log('value of x is; ' + x);
+// //assignment operators
+// let x = 10 + 5; 
+// console.log('value of x is; ' + x);
 
-x+= 10;
+// x+= 10;
 
-console.log('value of x after += is; ' + x);
+// console.log('value of x after += is; ' + x);
 
-x++;
-x--;
-x--;
-console.log("new value of x is; " + x);
+// x++;
+// x--;
+// x--;
+// console.log("new value of x is; " + x);
 
-//comparison operators
-console.log(ageAman > ageG);
-console.log(ageG >= 18);
+// //comparison operators
+// console.log(ageAman > ageG);
+// console.log(ageG >= 18);
 
-const isFullAge = ageG >= 18; 
-console.log(isFullAge);
+// const isFullAge = ageG >= 18; 
+// console.log(isFullAge);
 
 
-const myName = "Aman";
-console.log(myName.length);
+// const myName = "Aman";
+// console.log(myName.length);
 
-const avgAge = (ageAman + ageG) / 2;
-console.log("average age is:  "+ avgAge);
+// const avgAge = (ageAman + ageG) / 2;
+// console.log("average age is:  "+ avgAge);
 
-// precedence 
-let xx, y;
-xx = y = 25-10-5; 
-console.log(xx, y); 
+// // precedence 
+// let xx, y;
+// xx = y = 25-10-5; 
+// console.log(xx, y); 
+
+//
+console.log("coding challenge 1");
+
+let marks_mass = 90;
+let marks_height = 2.1;
+ 
+let johns_mass = 85;
+let johns_height = 2.05; 
+
+let marks_bmi = marks_mass / marks_height ** 2;
+console.log("marks bmi is; "+ marks_bmi);
+
+let johns_bmi = johns_mass / johns_height ** 2; 
+console.log("johns bmi is; "+ johns_bmi);
+
+console.log("is marks bmi is higher than johns bmi: " + (marks_bmi > johns_bmi));
+console.log("is johns bmi is higher than marks bmi: " + (marks_bmi < johns_bmi));
