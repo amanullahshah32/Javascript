52a491009ea8d5843578954341a0a4ca6942d9ea
amanullahshah32 <nafi17211@gmail.com>
2025-10-02 16:21:25 +0600

    function learned

--- 02_FUNDAMENTAL_PART_02/script.js
+++ 02_FUNDAMENTAL_PART_02/script.js
@@ -7,5 +7,32 @@ const passTest = true;
 if (passTest)  hasDriversLicense = true; 
 hasDriversLicense ? console.log('Aman can drive a car') : console.log('Aman cannot drive a car');
 
-const interface = 'audio';
+// const interface = 'audio';
 // console.log(interface); 
+
+// function
+
+console.log(`funtion start`);
+
+function logger()
+{
+    console.log('My name is Aman'); 
+}
+
+// calling / running / invoking function
+logger();
+
+function fruitProcessor( apples, oranges)
+{
+    console.log(apples, oranges);
+    const juice = `juice with ${apples} apples and ${oranges} orange `;
+    return juice;
+}
+
+const juice = fruitProcessor(5,2); 
+console.log(juice); 
+console.log(fruitProcessor(3,4)); 
+
+
+const appleOrangeJuice = fruitProcessor(2,4); 
+console.log(appleOrangeJuice);
