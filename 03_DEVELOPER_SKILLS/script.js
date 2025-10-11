2435ff3edd46d1fd19853283f591db26676632fc
amanullahshah32 <nafi17211@gmail.com>
2025-10-11 11:02:37 +0600

    prettier installed

--- 03_DEVELOPER_SKILLS/script.js
+++ 03_DEVELOPER_SKILLS/script.js
@@ -1,3 +1,26 @@
 // Remember, we're gonna use strict mode in all scripts now!
+
 'use strict';
 
+
+const x = '23';
+if (x === String(23)) console.log(23);
+
+const calcAge = (birthYear) => 
+{
+  return 2024 - birthYear;
+};
+console.log(calcAge(2003));
+
+
+
+// BUG
+// HACk
+// NOTE
+// TODO
+// REVIEW
+// FIXME
+// IMPORTANT
+// QUESTION
+// VIDEO
+// LEC
