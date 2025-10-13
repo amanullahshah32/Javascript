8ff31d509216c8ba929966ade7d85b8e87ae166b
amanullahshah32 <nafi17211@gmail.com>
2025-10-13 22:52:15 +0600

    debugging

--- 03_DEVELOPER_SKILLS/script.js
+++ 03_DEVELOPER_SKILLS/script.js
@@ -112,8 +112,16 @@ const measureKelvin = function ()
   const measurement = {
     type: 'temp', 
     unit: 'celsius', 
-    value: prompt('Degrees celsiul: ')
+    value: Number(prompt('Degrees celsius: '))
   }
-
+  console.log(measurement);
+  console.table(measurement);
+  console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
   const kelvin = measurement.value + 273;
+  return kelvin;
+
 }
+
+console.log(measureKelvin());
