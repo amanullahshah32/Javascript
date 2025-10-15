ce11cc36cb89272a6887acfa93fd79cb3a51099b
amanullahshah32 <nafi17211@gmail.com>
2025-10-15 13:08:14 +0600

    dom manupulation learned a  litttle bit

--- 05_Guess_My_Number/script.js
+++ 05_Guess_My_Number/script.js
@@ -3,3 +3,26 @@
 console.log(document.querySelector('.message').textContent);
 console.log(document.querySelector('.message'));
 console.log(document.querySelector('h1').textContent );
+
+
+//----------------
+
+console.log('-------------------    ');
+
+console.log(document.querySelector('.message').textContent);
+console.log(document.querySelector('.message').textContent = 'Correct Number');
+console.log(document.querySelector('.message').textContent);
+
+//------------
+
+console.log(document.querySelector('.number').textContent);
+console.log(document.querySelector('.number').textContent= 15);
+console.log(document.querySelector('.number').textContent);
+
+//-----------
+console.log(document.querySelector('.score').textContent = 25); 
+//-----------
+// console.log(document.querySelector('.guess').textContent);
+// console.log(document.querySelector('.guess').textContent=14);
+console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value = 90);
