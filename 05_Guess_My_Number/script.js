0d56b3a8bcdb4f0f389612bbc23ed6587f67c98f
amanullahshah32 <nafi17211@gmail.com>
2025-10-16 10:57:33 +0600

    new script file added

--- 05_Guess_My_Number/script.js
+++ 05_Guess_My_Number/script.js
@@ -1,30 +1,30 @@
-'use strict';
+// 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-console.log(document.querySelector('.message'));
-console.log(document.querySelector('h1').textContent );
+// console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message'));
+// console.log(document.querySelector('h1').textContent );
 
 
-//----------------
+// //----------------
 
-console.log('-------------------    ');
+// console.log('-------------------    ');
 
-console.log(document.querySelector('.message').textContent);
-console.log(document.querySelector('.message').textContent = 'Correct Number');
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent = 'Correct Number');
+// console.log(document.querySelector('.message').textContent);
 
-//------------
+// //------------
 
-console.log(document.querySelector('.number').textContent);
-console.log(document.querySelector('.number').textContent= 15);
-console.log(document.querySelector('.number').textContent);
+// console.log(document.querySelector('.number').textContent);
+// console.log(document.querySelector('.number').textContent= 15);
+// console.log(document.querySelector('.number').textContent);
 
-//-----------
-console.log(document.querySelector('.score').textContent = 25); 
-//-----------
-// console.log(document.querySelector('.guess').textContent);
-// console.log(document.querySelector('.guess').textContent=14);
-console.log(document.querySelector('.guess').value);
-console.log(document.querySelector('.guess').value = 90);
+// //-----------
+// console.log(document.querySelector('.score').textContent = 25); 
+// //-----------
+// // console.log(document.querySelector('.guess').textContent);
+// // console.log(document.querySelector('.guess').textContent=14);
+// console.log(document.querySelector('.guess').value);
+// console.log(document.querySelector('.guess').value = 90);
 
 
---
+++ 05_Guess_My_Number/script2.js
@@ -0,0 +1,8 @@
+'use strict';
+document.querySelector('body').style.backgroundColor = '#ce8c8cff';
+
+
+console.log(document.querySelector('.check').textContent);
+
+
+
