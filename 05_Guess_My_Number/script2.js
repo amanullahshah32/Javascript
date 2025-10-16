45f71118561642999d7f9e1f954ef7217be39d6f
amanullahshah32 <nafi17211@gmail.com>
2025-10-16 12:03:50 +0600

    logic modified

--- 05_Guess_My_Number/script2.js
+++ 05_Guess_My_Number/script2.js
@@ -3,8 +3,12 @@ document.querySelector('body').style.backgroundColor = '#ce8c8cff';
 
 
 console.log(document.querySelector('.check').textContent);
+
+let score =20;
 const secretNumber = Math.trunc(Math.random() * 20 +1);
-document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
+// document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.number').value = secretNumber;
 
 
 document.querySelector('.check').addEventListener('click', function()
@@ -16,14 +20,45 @@ document.querySelector('.check').addEventListener('click', function()
    {
     document.querySelector('.message').textContent = 'No Number isnterted';
    }
-   if(guess)
-   {
-    document.querySelector('.message').textContent = 'Number inserted: ' + guess + ' \n Number doesnt match';
-   }
-   if(guess === secretNumber)
+
+   else if(guess === secretNumber)
    {
     document.querySelector('.message').textContent = ' 👍 Number Matched 👍';
+    document.querySelector('body').style.backgroundColor = '#57e52cff';
+    document.querySelector('.number').textContent = secretNumber;
+    
    }
+   else if(guess > secretNumber)
+   {
+    if(score>1)
+    {
+        document.querySelector('.message').textContent = 'Number is higher';
+        score--;
+        document.querySelector('.score').textContent = score; 
+    }
+    else{
+        document.querySelector('.message').textContent = '😔 You lost the game 😔';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+
+    }
+   else if(guess < secretNumber)
+   {
+    if(score>1)
+    {
+        document.querySelector('.message').textContent = 'Number is lower';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+    else{
+        document.querySelector('.message').textContent = '😔 You lost the game 😔';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+ 
+   }
+
 })
 
 
