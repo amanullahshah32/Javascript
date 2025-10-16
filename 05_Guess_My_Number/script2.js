e9e87d565cb34cc299ea24f2a13989a1d25b221e
amanullahshah32 <nafi17211@gmail.com>
2025-10-16 11:30:01 +0600

    logic added

--- 05_Guess_My_Number/script2.js
+++ 05_Guess_My_Number/script2.js
@@ -3,6 +3,27 @@ document.querySelector('body').style.backgroundColor = '#ce8c8cff';
 
 
 console.log(document.querySelector('.check').textContent);
+const secretNumber = Math.trunc(Math.random() * 20 +1);
+document.querySelector('.number').textContent = secretNumber;
 
+
+document.querySelector('.check').addEventListener('click', function()
+{
+   const guess = Number(document.querySelector('.guess').value);  
+   console.log(guess);
+
+   if(!guess)
+   {
+    document.querySelector('.message').textContent = 'No Number isnterted';
+   }
+   if(guess)
+   {
+    document.querySelector('.message').textContent = 'Number inserted: ' + guess + ' \n Number doesnt match';
+   }
+   if(guess === secretNumber)
+   {
+    document.querySelector('.message').textContent = ' 👍 Number Matched 👍';
+   }
+})
 
 
