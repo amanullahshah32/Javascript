75b3264e22b89116433744d7ff512bc98dcac659
amanullahshah32 <nafi17211@gmail.com>
2025-10-18 16:54:56 +0600

    button for close and open

--- 06_MODAL/script.js
+++ 06_MODAL/script.js
@@ -1,13 +1,28 @@
-'use strict';
+"use strict";
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 
 console.log(btnsOpenModal);
 
-for(let i = 0; i< btnsOpenModal.length; i++)
-{
-    console.log(btnsOpenModal[i]);
+
+// buttons open modal
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 }
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+
+//button close modal
+const closeModal = () =>
+{
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
