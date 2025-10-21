42b013ec0f6c6118a270d94e226157c0f5aa0edb
amanullahshah32 <nafi17211@gmail.com>
2025-10-22 00:42:31 +0600

    score saving for player 1

--- 07_PIG_GAME/script.js
+++ 07_PIG_GAME/script.js
@@ -15,12 +15,31 @@ score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add('hidden'); 
 
+let currentScore  = 0; 
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
+
 //rolling dice functionality
 btnRoll.addEventListener('click', function()
 {
     // 1. generating a renadom dice roll
     const dice = Number(Math.trunc(Math.random() *6) + 1); 
     //2. display dice
-
+    diceEL.src = 'dice_images/dice-' + dice + '.png';
+    // console.log(`pressed ${dice}`);
+    diceEL.classList.remove('hidden');
     //. 3. check for rolled 1: if ture, switch to next palyer
+    if(dice !==1)
+    {
+        // add dice to current score
+        currentScore = currentScore + dice;
+        current0EL.textContent = currentScore; // change later
+
+    }
+    else{
+        // switch to next player
+        current0EL.textContent = 0; 
+        currentScore = 0;
+
+    }
 })
