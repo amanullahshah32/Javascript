c5e763d1957b059719895ef5a465e2416d4313f7
amanullahshah32 <nafi17211@gmail.com>
2025-10-22 00:32:17 +0600

    scripting

--- 07_PIG_GAME/script.js
+++ 07_PIG_GAME/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// selecting elements
+const score0EL = document.getElementById('score--0');
+const score1EL = document.getElementById('score--1'); 
+
+const diceEL = document.querySelector('.dice'); 
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold =document.querySelector('.btn--hold');
+
+//starting conditions
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add('hidden'); 
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function()
+{
+    // 1. generating a renadom dice roll
+    const dice = Number(Math.trunc(Math.random() *6) + 1); 
+    //2. display dice
+
+    //. 3. check for rolled 1: if ture, switch to next palyer
+})
