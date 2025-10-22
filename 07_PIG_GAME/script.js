afc94dcc347c60ffe4c88afc7d49164a383eeffa
amanullahshah32 <nafi17211@gmail.com>
2025-10-22 13:59:52 +0600

    player switching logic impelmented

--- 07_PIG_GAME/script.js
+++ 07_PIG_GAME/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // selecting elements
+const player0EL = document.querySelector('.playre--0');
+const player1EL = document.querySelector('.playre--1');
 const score0EL = document.getElementById('score--0');
 const score1EL = document.getElementById('score--1'); 
 
@@ -15,7 +17,11 @@ score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add('hidden'); 
 
+
+const scores = [0, 0];
 let currentScore  = 0; 
+let activePlayer = 0;
+
 const current0EL = document.getElementById('current--0');
 const current1EL = document.getElementById('current--1');
 
@@ -33,42 +39,17 @@ btnRoll.addEventListener('click', function()
     {
         // add dice to current score
         currentScore = currentScore + dice;
-        current0EL.textContent = currentScore; // change later
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 
     }
     else{
         // switch to next player
-        current0EL.textContent = 0; 
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
         currentScore = 0;
-        switchPlayer();
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        activePlayer = activePlayer === 0 ? 1 : 0; 
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
 
     }
 })
 
-const switchPlayer = function()
-{
-    btnRoll.addEventListener('click', function()
-{
-    // 1. generating a renadom dice roll
-    const dice = Number(Math.trunc(Math.random() *6) + 1); 
-    //2. display dice
-    diceEL.src = 'dice_images/dice-' + dice + '.png';
-    // console.log(`pressed ${dice}`);
-    diceEL.classList.remove('hidden');
-    //. 3. check for rolled 1: if ture, switch to next palyer
-    if(dice !==1)
-    {
-        // add dice to current score
-        currentScore = currentScore + dice;
-        current1EL.textContent = currentScore; // change later
-
-    }
-    else{
-        // switch to next player
-        current1EL.textContent = 0; 
-        currentScore = 0;
-
-    }
-})
-
-}
