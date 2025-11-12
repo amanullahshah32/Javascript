c13f7e2b6f0e657c55be8a2d70800e8b4fb8d50d
amanullahshah32 <nafi17211@gmail.com>
2025-11-12 11:36:01 +0600

    scopping practiced

--- 08_Behind_the_Scene/script.js
+++ 08_Behind_the_Scene/script.js
@@ -3,8 +3,26 @@
 function calcAge(birthYear) {
   const age = 2025 - birthYear;
   console.log(firstName);
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = false;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    } else {
+      // var millenial = true;
+      const str2 = `You are not a millenial ${firstName}`;
+      console.log(str2);
+    }
+  }
+  printAge();
+  console.log(millenial);
+  // console.log(str);
+  // console.log(str2);
   return age;
 }
 
-const firstName = "AmanUllah";
-calcAge(2001);
+const firstName = "AmanUllahShah";
+calcAge(1986);
+console.log(age);
