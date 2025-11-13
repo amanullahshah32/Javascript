bd56ea2f57d47d79cd7610ab58962c04b3422dbd
amanullahshah32 <nafi17211@gmail.com>
2025-11-13 20:52:04 +0600

    variable hoisting

--- 08_Behind_the_Scene/script.js
+++ 08_Behind_the_Scene/script.js
@@ -1,28 +1,72 @@
 "use strict";
 
-function calcAge(birthYear) {
-  const age = 2025 - birthYear;
-  console.log(firstName);
-  function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
-    console.log(output);
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = false;
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
-    } else {
-      // var millenial = true;
-      const str2 = `You are not a millenial ${firstName}`;
-      console.log(str2);
-    }
-  }
-  printAge();
-  console.log(millenial);
-  // console.log(str);
-  // console.log(str2);
-  return age;
+// function calcAge(birthYear) {
+//   const age = 2025 - birthYear;
+//   console.log(firstName);
+//   function printAge() {
+//     const output = `You are ${age}, born in ${birthYear}`;
+//     console.log(output);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = false;
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
+//     } else {
+//       // var millenial = true;
+//       const str2 = `You are not a millenial ${firstName}`;
+//       console.log(str2);
+//     }
+//   }
+//   printAge();
+//   console.log(millenial);
+//   // console.log(str);
+//   // console.log(str2);
+//   return age;
+// }
+
+// const firstName = "AmanUllahShah";
+// calcAge(1986);
+// console.log(age);
+// console.log(`turned off git warning`);
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "aman";
+let job = "developer";
+const year = 1986;
+
+// // functions
+// console.log(addDecl(2,3));
+// console.log(addExpr(2,3));
+// console.log(addArrow(2,3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+//example
+console.log(typeof(undefined));
+console.log(undefined);
+if(!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart()
+{
+  console.log("All products deleted!");
 }
 
-const firstName = "AmanUllahShah";
-calcAge(1986);
-console.log(age);
+var x = 1;
+let y = 22;
+const z = 245;
+
+console.log(x === window.x);
+console.log(y === window.x);
+console.log(z === window.x);
+
