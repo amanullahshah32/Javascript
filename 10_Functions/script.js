47f71007e5ac7d506be7d46d38d3b9bf770c6c7c
amanullahshah32 <nafi17211@gmail.com>
2025-12-01 22:29:41 +0600

    pass by value and pass by reference

--- 10_Functions/script.js
+++ 10_Functions/script.js
@@ -19,3 +19,47 @@ console.log(booking);
 
 createBooking('lh32', undefined, 800);
 createBooking('lh354');
+
+
+// how passing arguments works: value vs reference
+
+console.log(`-----Passing Arguments: Value vs Reference-----`);
+
+const flight = 'LH234';
+const Aman= {
+    name: 'Aman Ullah',
+    passport: 345324342342,
+
+}
+
+const checkIn = function(flightNum=343, passenger)
+{
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    // check the passport validity
+    if(passenger.passport === 345324342342)
+    {
+        console.log('Checked In');
+    }
+    else{
+        console.log('Wrong Passport!');
+    }
+}
+
+checkIn(flight, Aman);
+console.log(flight);
+console.log(Aman);
+
+// is the same as doing...
+const flightNum = flight;
+const passenger = Aman;
+
+const newPassport = function(person)
+{
+    person.passport = Math.trunc(Math.random() * 100000000000);
+    console.log(person);
+}
+
+newPassport(Aman);
+checkIn(flight, Aman);
