7bba8e7902b867a118a4341e440ba11323d7b961
amanullahshah32 <nafi17211@gmail.com>
2025-12-01 22:18:56 +0600

    default parameter learned

--- 10_Functions/script.js
+++ 10_Functions/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const booking = [];
+const createBooking = function(flightNum, numPassengers = 1, price = 200)
+{
+    
+    const bookingObj ={
+        flightNum,
+        numPassengers,
+        price: price || 199 * numPassengers,
+    }
+    console.log(bookingObj);
+    booking.push(bookingObj);
+
+}
+
+createBooking("LH123");
+console.log(booking);
+
+createBooking('lh32', undefined, 800);
+createBooking('lh354');
