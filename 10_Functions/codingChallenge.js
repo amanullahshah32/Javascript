324efcd0cba8b586657379fef75142a3073c1b14
amanullahshah32 <nafi17211@gmail.com>
2025-12-04 21:34:15 +0600

    coding challenge done

--- 10_Functions/codingChallenge.js
+++ 10_Functions/codingChallenge.js
@@ -5,7 +5,7 @@ const poll = {
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
-//   this.displayResults();
+  //   this.displayResults();
 };
 
 // 1. create registerNewAnswer method
@@ -24,8 +24,8 @@ poll.registerNewAnswer = function () {
   }
   // Call displayResults after each answer
   this.displayResults();
-  this.displayResults2('array');
-  this.displayResults2('string');
+  this.displayResults2("array");
+  this.displayResults2("string");
 };
 
 // 3. create displayResults method to show options with their vote counts
@@ -62,21 +62,14 @@ document
   .addEventListener("click", poll.registerNewAnswer.bind(poll));
 // console.log(poll);
 
-
 // 3. a method to display results in array and string format
-poll.displayResults2 = function(type)
-{
-    if(type === 'array')
-    {
-        console.log(this.answers);
-    }
-    
-    else if(type === 'string')
-    {
-        console.log(` Poll Restults are: ${this.answers.join(',' )}`);
-        
-    }
-}// 5. bonus: use displayResults method to display results of 2 arrays
+poll.displayResults2 = function (type) {
+  if (type === "array") {
+    console.log(this.answers);
+  } else if (type === "string") {
+    console.log(` Poll Restults are: ${this.answers.join(",")}`);
+  }
+}; // 5. bonus: use displayResults method to display results of 2 arrays
 
 // BONUS: Using .call() to set 'this' keyword manually
 // Remember: .call() lets us control what 'this' refers to!
@@ -89,9 +82,35 @@ const testData2 = [1, 5, 3, 9, 6, 1];
 // We pass an object { answers: testData1 } as 'this'
 
 console.log(`--- Bonus: Test Data 1 ---`);
-poll.displayResults2.call({ answers: testData1 }, 'array');   // [5, 2, 3]
-poll.displayResults2.call({ answers: testData1 }, 'string');  // 5, 2, 3
+poll.displayResults2.call({ answers: testData1 }, "array"); // [5, 2, 3]
+poll.displayResults2.call({ answers: testData1 }, "string"); // 5, 2, 3
 
 console.log(`--- Bonus: Test Data 2 ---`);
-poll.displayResults2.call({ answers: testData2 }, 'array');   // [1, 5, 3, 9, 6, 1]
-poll.displayResults2.call({ answers: testData2 }, 'string');  // 1, 5, 3, 9, 6, 1
+poll.displayResults2.call({ answers: testData2 }, "array"); // [1, 5, 3, 9, 6, 1]
+poll.displayResults2.call({ answers: testData2 }, "string"); // 1, 5, 3, 9, 6, 1
+
+// coding challenge 2
+console.log(`coding challenge 2 ----  --`);
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";  // header is born here 
+
+  // CHALLENGE: Change h1 color to blue when BODY is clicked
+  // WITHOUT selecting h1 again!
+  
+  // The 'header' variable is defined above, and the callback function
+  // can still access it even after the IIFE finishes running!
+  // This is called a CLOSURE!
+  
+  document.querySelector('body').addEventListener('click', function() {
+    // this callback function was born inside the iife, so it can remeber the header forever
+    header.style.color = 'blue';  // Using 'header' variable from outer scope!
+  });
+
+})();
+
+
+// // 1. listener function to change header color to blue on click without selecting the h1 element again
+// Closures = Functions remember their birthplace! 🎂
+console.log(`Closures = Functions remember their birthplace! 🎂`);
