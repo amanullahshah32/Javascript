2aaf9008245681689947392acb5472a8fb18a882
amanullahshah32 <nafi17211@gmail.com>
2025-12-08 19:57:23 +0600

    bankist app started

--- 11_Arrays_Bankist/script.js
+++ 11_Arrays_Bankist/script.js
@@ -61,6 +61,23 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+
+// project bankist app
+const displayMovements = function (movements, sort = false){
+  containerMovements.innerHTML = ''; // clearing the container before adding new elements
+   movements.forEach(function(mov, index)
+  {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `<div class="movements__row">
+                    <div class="movements__type movements__type--${type}">${index + 1}: ${type}</div>
+                    <div class="movements__value"> $ ${mov<0 ? (Math.abs(mov)) : mov}</div>
+                  </div>`;
+
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  })
+}
+
+displayMovements(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,7 +91,7 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
+console.log(`Array methods practice:`);
 // Array method practice
 // slice method
 let arr = ["a", "b", "c", "d", "e"];
