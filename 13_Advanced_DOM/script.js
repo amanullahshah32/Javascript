2450abe4446f6f7f5df5f40351d357f93ec78071
amanullahshah32 <nafi17211@gmail.com>
2026-01-01 21:05:23 +0600

    event deligation learned

--- 13_Advanced_DOM/script.js
+++ 13_Advanced_DOM/script.js
@@ -1,67 +1,63 @@
-'use strict';
+"use strict";
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
 const openModal = function (event) {
   event.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 // Alternatively, using a for loop:
 //
 // for (let i = 0; i < btnsOpenModal.length; i++)
 //   btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
-
-
 
 // selecting elements
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
+const header = document.querySelector(".header");
+console.log(document.querySelector(".header"));
+console.log(document.querySelectorAll(".header"));
+console.log(document.querySelectorAll(".section"));
 
-const header = document.querySelector('.header');
-console.log(document.querySelector('.header'));
-console.log(document.querySelectorAll('.header'));
-console.log(document.querySelectorAll('.section'));
+console.log(document.getElementById("section--1"));
+console.log(document.getElementsByTagName("button"));
+console.log(document.getElementsByTagName("button"));
 
-console.log(document.getElementById('section--1'));
-console.log(document.getElementsByTagName('button'));
-console.log(document.getElementsByTagName('button'));
-
-console.log(document.getElementsByClassName('btn'));
-
+console.log(document.getElementsByClassName("btn"));
 
 // creating and inserting elemenets
 // insertAdjacentHTML
-const message = document.createElement('div'); // creates a div element
-message.classList.add('cookie-message'); // adds a class to the div element
+const message = document.createElement("div"); // creates a div element
+message.classList.add("cookie-message"); // adds a class to the div element
 // message.textContent = ' We use cookies for imporved functionality and analytics. '; // adds text content to the div element
 
-
-message.innerHTML = 'We use cookies for imporved functionality and analytics. <button class="btn btn--close-cookie"> GOT IT! </button>'; // adds HTML content to the div element
+message.innerHTML =
+  'We use cookies for imporved functionality and analytics. <button class="btn btn--close-cookie"> GOT IT! </button>'; // adds HTML content to the div element
 
 header.prepend(message); // adds the message element as the first child of the header element
 header.append(message); // moves the message element as the last child of the header element
@@ -71,22 +67,28 @@ header.append(message); // moves the message element as the last child of the he
 // header.insertAdjacentHTML('afterbegin', '<p style="color: blue; font-weight: bold;">This is a demo of insertAdjacentHTML!</p> <button class="btn btn--close-cookie"> CLOSE </button>');
 // Try changing 'afterbegin' to 'beforeend', 'beforebegin', or 'afterend' to see different results.
 
-document.querySelector('.btn--close-cookie').addEventListener('click', function(e){
-  e.preventDefault();
-  message.remove();
-  message.parentElement.removeChild(message); // old way of removing an element
-})
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    message.remove();
+    message.parentElement.removeChild(message); // old way of removing an element
+  });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
-btnScrollTo.addEventListener('click', function(e){
+btnScrollTo.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
-  console.log(e.target.getBoundingClientRect(   ));
+  console.log(e.target.getBoundingClientRect());
 
-  console.log('current scroll (x/y', window.pageXOffset, window.pageYOffset  );
-  console.log('height/width viewpost', document.documentElement.clientHeight, document.documentElement.clientWidth );
+  console.log("current scroll (x/y", window.pageXOffset, window.pageYOffset);
+  console.log(
+    "height/width viewpost",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
 
   // // scrooling
   // window.scrollTo(s1coords.left + window.pageXOffset , s1coords.top + window.pageYOffset);
@@ -97,13 +99,46 @@ btnScrollTo.addEventListener('click', function(e){
   //   behavior: 'smooth',
   // });
   // modern way
-  section1.scrollIntoView({behavior: 'smooth' }); // scrolls to section1 smoothly and its so easy :)
-})
+  section1.scrollIntoView({ behavior: "smooth" }); // scrolls to section1 smoothly and its so easy :)
+});
+
+// smooth scrolling on feature links
+// document.querySelectorAll(".nav__link").forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     const sectionId = document.querySelector(id);
+//     console.log(`clicked: ${this.textContent}`);
+//     if (sectionId) {
+//       sectionId.scrollIntoView({ behavior: "smooth" });
+//     }
+//   });
+// });
+
+
+// Event delegation for smooth scrolling on feature links
+// 1. add event listener to common parent elements
+// 2. determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  e.preventDefault();
+  console.log(e.target);
+  console.log(this);
+
+  // matching strategy
+  if(e.target.classList.contains('nav__link'))
+  {
+    // e.preventDefault();
+    // console.log(this);
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+};
+});
+
 
 // element.insertAdjacentHTML('beforeend', '<p>Hello!</p>');
 
-
-const h1 = document.querySelector('h1');
+const h1 = document.querySelector("h1");
 
 // h1.addEventListener('mouseenter', function(e)
 // {
@@ -119,38 +154,34 @@ const h1 = document.querySelector('h1');
 //   alert('You are reading the heading by addEventListener:D')
 //   h1.removeEventListener('mouseenter', h1Alerts);
 // };
-  
+
 // h1.addEventListener('mouseenter', h1Alerts);
 
 // setTimeout( ()=> h1.removeEventListener('mouseenter', h1Alerts), 3000);
 
-
 // random colors
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min +1) + min);
-const randomColor = () => `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`;
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
 
 console.log(randomColor());
 
-document.querySelector('.nav__link').addEventListener('click', function(e)
-{
-  console.log('clicked');
-  this.style.backgroundColor = randomColor();
-    console.log('link', e.target, e.currentTarget);
+// document.querySelector(".nav__link").addEventListener("click", function (e) {
+//   console.log("clicked");
+//   this.style.backgroundColor = randomColor();
+//   // console.log("link", e.target, e.currentTarget);
 
-    e.stopPropagation(); // stops the event bubbling
-})
-document.querySelector('.nav__links').addEventListener('click', function(e)
-{
-  this.style.backgroundColor = randomColor();
-  console.log('container', e.target, e.currentTarget);
-      // e.stopPropagation(); // stops the event bubbling
-
-})
-document.querySelector('.nav').addEventListener('click', function(e)
-{
-  console.log('clicked');
-  this.style.backgroundColor = randomColor();
-    console.log('nav', e.target, e.currentTarget);
-
-});  // 'true' capturing phase)
+//   e.stopPropagation(); // stops the event bubbling
+// });
+// document.querySelector(".nav__links").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   // console.log("container", e.target, e.currentTarget);
+//   // e.stopPropagation(); // stops the event bubbling
+// });
+// document.querySelector(".nav").addEventListener("click", function (e) {
+//   console.log("clicked");
+//   this.style.backgroundColor = randomColor();
+//   // console.log("nav", e.target, e.currentTarget);
+// }); // 'true' capturing phase)
