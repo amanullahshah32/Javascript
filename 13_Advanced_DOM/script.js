0cad6719a81844ea1b8d1fc9fb7a76ddb8722c1a
amanullahshah32 <nafi17211@gmail.com>
2026-01-09 20:13:25 +0600

    left button isnt working

--- 13_Advanced_DOM/script.js
+++ 13_Advanced_DOM/script.js
@@ -367,7 +367,50 @@ imgTargets.forEach( img => imgObserver.observe(img));
 
 // Slider component
 const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0; 
+const maxSlide = slides.length;
+
+const slider = document.querySelector('.slider');
+slider.style.transform = 'scale(0.4) translateX(-800px)';
+slider.style.overflow = 'visible';
+
 slides.forEach((slide, index) => {
   slide.style.transform = `translateX(${100 * index}%)`
   // 0%, 100%, 200%, 300%
 })
+
+
+const goToSlide = function(slide){
+    slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - curSlide)}%)`
+  });
+}
+
+goToSlide(0);
+
+// Next slide
+
+const nextSlide = function(){
+  if(curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+}
+btnRight.addEventListener('click', function(){
+  nextSlide();
+});
+
+// Previous slide
+btnLeft.addEventListener('click', function(){
+  if(curSlide === 0){
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+});
