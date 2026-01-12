9aee0328513d6101046978edf2cb80a7379d9b1a
amanullahshah32 <nafi17211@gmail.com>
2026-01-12 14:10:09 +0600

    problme 20 solved

--- leetcodeJS/problems/twoSum.js
+++ leetcodeJS/problems/twoSum.js
@@ -1,122 +1,154 @@
-"use strict";
+// "use strict";
 
-// console.log(`Two Sum - LeetCode Problem`);
+// // console.log(`Two Sum - LeetCode Problem`);
 
-// const twoSum = function (nums, target) {
-//   let result = [];
+// // const twoSum = function (nums, target) {
+// //   let result = [];
 
-//   // Use map with all parameters: value, index, array
-//   nums.map((num, index, array) => {
-//     // Calculate what number we need to find
-//     const complement = target - num;
+// //   // Use map with all parameters: value, index, array
+// //   nums.map((num, index, array) => {
+// //     // Calculate what number we need to find
+// //     const complement = target - num;
 
-//     // Search for complement in remaining part of array
-//     const complementIndex = array.indexOf(complement, index + 1);
+// //     // Search for complement in remaining part of array
+// //     const complementIndex = array.indexOf(complement, index + 1);
 
-//     // If complement found and we haven't found result yet
-//     if (complementIndex !== -1 && result.length === 0) {
-//       result = [index, complementIndex];
-//     }
+// //     // If complement found and we haven't found result yet
+// //     if (complementIndex !== -1 && result.length === 0) {
+// //       result = [index, complementIndex];
+// //     }
 
-//     return null; // map needs return value
-//   });
+// //     return null; // map needs return value
+// //   });
 
-//   return result;
-// };
+// //   return result;
+// // };
 
-// // Test Cases
-// const nums = [2, 7, 11, 15];
-// const target = 9;
-// const result = twoSum(nums, target);
-// console.log(`Result: [${result}]`);
-// console.log(
-//   `✅ Test Passed: nums[${result[0]}] + nums[${result[1]}] = ${
-//     nums[result[0]]
-//   } + ${nums[result[1]]} = ${target}`
-// );
+// // // Test Cases
+// // const nums = [2, 7, 11, 15];
+// // const target = 9;
+// // const result = twoSum(nums, target);
+// // console.log(`Result: [${result}]`);
+// // console.log(
+// //   `✅ Test Passed: nums[${result[0]}] + nums[${result[1]}] = ${
+// //     nums[result[0]]
+// //   } + ${nums[result[1]]} = ${target}`
+// // );
 
-// // 13. Roman to Integer
-// const romanToInt = function (string) {
-//   let total = 0;
-//   const romanChar = string.split(""); // Don't skip first character
+// // // 13. Roman to Integer
+// // const romanToInt = function (string) {
+// //   let total = 0;
+// //   const romanChar = string.split(""); // Don't skip first character
 
-//   romanChar.forEach((char, indexOfChar) => {
-//     const currentValue = symbols[char];
-//     const nextValue = symbols[romanChar[indexOfChar + 1]];
+// //   romanChar.forEach((char, indexOfChar) => {
+// //     const currentValue = symbols[char];
+// //     const nextValue = symbols[romanChar[indexOfChar + 1]];
 
-//     // Key logic: If current < next, subtract; otherwise add
-//     if (nextValue && currentValue < nextValue) {
-//       total -= currentValue; // Subtract (like I in IV)
-//     } else {
-//       total += currentValue; // Add normally
-//     }
-//   });
+// //     // Key logic: If current < next, subtract; otherwise add
+// //     if (nextValue && currentValue < nextValue) {
+// //       total -= currentValue; // Subtract (like I in IV)
+// //     } else {
+// //       total += currentValue; // Add normally
+// //     }
+// //   });
 
-//   return total;
-// };
+// //   return total;
+// // };
 
-// const symbols = {
-//   I: 1,
-//   V: 5,
-//   X: 10,
-//   L: 50,
-//   C: 100,
-//   D: 500,
-//   M: 1000,
-// };
-// console.log(romanToInt("MCMXCIV")); // 58
+// // const symbols = {
+// //   I: 1,
+// //   V: 5,
+// //   X: 10,
+// //   L: 50,
+// //   C: 100,
+// //   D: 500,
+// //   M: 1000,
+// // };
+// // console.log(romanToInt("MCMXCIV")); // 58
 
+// // // console.log(romanToInt("LVIII")); // 58
 // // console.log(romanToInt("LVIII")); // 58
-// console.log(romanToInt("LVIII")); // 58
 
 
-// 14. Longest Common Prefix
-// const longestCommonPrefix = function(strs) {
-//   if (strs.length === 0) return "";
+// // 14. Longest Common Prefix
+// // const longestCommonPrefix = function(strs) {
+// //   if (strs.length === 0) return "";
   
-//   let prefix = strs[0];  // Start with first string as prefix
-//   // console.log(strs[1].indexOf(prefix));
-//   for (let i = 1; i < strs.length; i++) {
-//     while (strs[i].indexOf(prefix) !== 0) {
-//       prefix = prefix.slice(0, -1);  // Remove last character
-//       if (prefix === "") return "";
-//     }
+// //   let prefix = strs[0];  // Start with first string as prefix
+// //   // console.log(strs[1].indexOf(prefix));
+// //   for (let i = 1; i < strs.length; i++) {
+// //     while (strs[i].indexOf(prefix) !== 0) {
+// //       prefix = prefix.slice(0, -1);  // Remove last character
+// //       if (prefix === "") return "";
+// //     }
+// //   }
+  
+// //   return prefix;
+// // };
+
+// // const strings = ["flower", "flow", "flight"];
+// // console.log(longestCommonPrefix(strings)); // "fl"
+
+
+// // 11. Container With Most Water
+// console.log(`Container With Most Water - LeetCode Problem 11`);
+
+// const maxArea = function(height) {
+
+//   const length = height.length;
+//   let maxArea = 0;
+//   let left = 0;
+//   let right = length - 1;
+
+//   while (left < right){
+//     let width = right - left; 
+//     const currentHeight = Math.min(height[left] , height[right]);
+//     const area = width * currentHeight; 
+//     maxArea = Math.max(maxArea, area);
+
+//     // move to the right 
+//     if(height[left] < height [right])
+//       left ++; 
+//     else
+//       right --;
+    
 //   }
-  
-//   return prefix;
+//   return maxArea;
+
 // };
 
-// const strings = ["flower", "flow", "flight"];
-// console.log(longestCommonPrefix(strings)); // "fl"
+// console.log(maxArea([1,8,6,2,5,4,8,3,7])); // 49  // 0 to 8
+// console.log(maxArea([1,1])); // 1  // 0 to 1
+// console.log(maxArea([1,2,1])); // 1  // 0 to 1
 
 
-// 11. Container With Most Water
-console.log(`Container With Most Water - LeetCode Problem 11`);
+// 20. Valid Parentheses
+console.log(`Valid Parentheses - LeetCode Problem 20`);
 
-const maxArea = function(height) {
+var isValid = function(parentheses) {
+    const pairs = {
+      '(' : ')',
+      '{' : '}',
+      '[' : ']',
+    }
 
-  const length = height.length;
-  let maxArea = 0;
-  let left = 0;
-  let right = length - 1;
+    const stack = [];
+    // console.log(pairs[parentheses[0]]);
+    for ( let char of parentheses){
+      if (pairs[char]){
+        stack.push(pairs[char]);
+      }
+      else if ( stack.pop() !== char){
+          return false;
+        }
+      
 
-  while (left < right){
-    let width = right - left; 
-    const currentHeight = Math.min(height[left] , height[right]);
-    const area = width * currentHeight; 
-    maxArea = Math.max(maxArea, area);
-
-    // move to the right 
-    if(height[left] < height [right])
-      left ++; 
-    else
-      right --;
-    
-  }
-  return maxArea;
+    }
+    if (stack.length === 0 ) 
+      return true;
+    else  return false; 
 
 };
 
-console.log(maxArea([1,8,6,2,5,4,8,3,7])); // 49  // 0 to 8
-console.log(maxArea([1,1])); // 1  // 0 to 1
-console.log(maxArea([1,2,1])); // 1  // 0 to 1
+console.log(isValid("()")); // true
+console.log(isValid("()[]{}")); // true
