cbb7a6b55e0c70b27418083ca52d249af208ca32
amanullahshah32 <nafi17211@gmail.com>
2026-01-12 21:27:13 +0600

    sorted array merge failed

--- leetcodeJS/problems/twoSum.js
+++ leetcodeJS/problems/twoSum.js
@@ -69,11 +69,10 @@
 // // // console.log(romanToInt("LVIII")); // 58
 // // console.log(romanToInt("LVIII")); // 58
 
-
 // // 14. Longest Common Prefix
 // // const longestCommonPrefix = function(strs) {
 // //   if (strs.length === 0) return "";
-  
+
 // //   let prefix = strs[0];  // Start with first string as prefix
 // //   // console.log(strs[1].indexOf(prefix));
 // //   for (let i = 1; i < strs.length; i++) {
@@ -82,13 +81,12 @@
 // //       if (prefix === "") return "";
 // //     }
 // //   }
-  
+
 // //   return prefix;
 // // };
 
 // // const strings = ["flower", "flow", "flight"];
 // // console.log(longestCommonPrefix(strings)); // "fl"
-
 
 // // 11. Container With Most Water
 // console.log(`Container With Most Water - LeetCode Problem 11`);
@@ -101,17 +99,17 @@
 //   let right = length - 1;
 
 //   while (left < right){
-//     let width = right - left; 
+//     let width = right - left;
 //     const currentHeight = Math.min(height[left] , height[right]);
-//     const area = width * currentHeight; 
+//     const area = width * currentHeight;
 //     maxArea = Math.max(maxArea, area);
 
-//     // move to the right 
+//     // move to the right
 //     if(height[left] < height [right])
-//       left ++; 
+//       left ++;
 //     else
 //       right --;
-    
+
 //   }
 //   return maxArea;
 
@@ -121,34 +119,86 @@
 // console.log(maxArea([1,1])); // 1  // 0 to 1
 // console.log(maxArea([1,2,1])); // 1  // 0 to 1
 
-
 // 20. Valid Parentheses
-console.log(`Valid Parentheses - LeetCode Problem 20`);
+// console.log(`Valid Parentheses - LeetCode Problem 20`);
 
-var isValid = function(parentheses) {
-    const pairs = {
-      '(' : ')',
-      '{' : '}',
-      '[' : ']',
+// var isValid = function(parentheses) {
+//     const pairs = {
+//       '(' : ')',
+//       '{' : '}',
+//       '[' : ']',
+//     }
+
+//     const stack = [];
+//     // console.log(pairs[parentheses[0]]);
+//     for ( let char of parentheses){
+//       if (pairs[char]){
+//         stack.push(pairs[char]);
+//       }
+//       else if ( stack.pop() !== char){
+//           return false;
+//         }
+
+//     }
+//     if (stack.length === 0 )
+//       return true;
+//     else  return false;
+
+// };
+
+// console.log(isValid("()")); // true
+// console.log(isValid("()[]{}")); // true
+
+// 21. Merge Two Sorted Lists
+console.log(`Merge Two Sorted Lists - LeetCode Problem 21`);
+
+// Correct solution for linked lists:
+const mergeTwoLists = function (list1, list2) {
+  // Create a dummy node to simplify logic
+  const dummy = { val: 0, next: null };
+  let current = dummy;
+
+  // Traverse both lists and merge
+  while (list1 !== null && list2 !== null) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
     }
+    current = current.next;
+  }
 
-    const stack = [];
-    // console.log(pairs[parentheses[0]]);
-    for ( let char of parentheses){
-      if (pairs[char]){
-        stack.push(pairs[char]);
-      }
-      else if ( stack.pop() !== char){
-          return false;
-        }
-      
+  // Attach remaining nodes (if any)
+  current.next = list1 !== null ? list1 : list2;
 
-    }
-    if (stack.length === 0 ) 
-      return true;
-    else  return false; 
-
+  return dummy.next;
 };
 
-console.log(isValid("()")); // true
-console.log(isValid("()[]{}")); // true
+// Helper function to create linked list from array
+function createList(arr) {
+  if (arr.length === 0) return null;
+  const head = { val: arr[0], next: null };
+  let current = head;
+  for (let i = 1; i < arr.length; i++) {
+    current.next = { val: arr[i], next: null };
+    current = current.next;
+  }
+  return head;
+}
+
+// Helper function to print linked list
+function printList(head) {
+  const result = [];
+  while (head !== null) {
+    result.push(head.val);
+    head = head.next;
+  }
+  return result;
+}
+
+const list1 = createList([1, 2, 4]);
+const list2 = createList([1, 3, 4]);
+const merged = mergeTwoLists(list1, list2);
+console.log(printList(merged)); // [1,1,2,3,4,4]
