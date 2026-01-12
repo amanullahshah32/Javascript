dc090336a245383d0216b8f17066f665ebf0cd85
amanullahshah32 <nafi17211@gmail.com>
2026-01-12 22:05:55 +0600

    linkedlist class

--- leetcodeJS/linkedList/linkedList.js
+++ leetcodeJS/linkedList/linkedList.js
@@ -1,3 +1,29 @@
 'use strict';
 
 console.log(`Linked List - LeetCode Problem`);
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmplty() {
+        return this.size ===0;
+
+    }
+    getSize(){
+        return this.size;
+    }
+}
+
+const list = new LinkedList();
+console.log(`list is emply? ${list.isEmplty()}`); // true
+console.log(`list size: ${list.getSize()}`); // 0
