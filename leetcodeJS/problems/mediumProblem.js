2e3228d37c702d9b9e750e29adf3ae32fc07ba3a
amanullahshah32 <nafi17211@gmail.com>
2026-01-20 00:14:53 +0600

    problem 15 solve

--- leetcodeJS/problems/mediumProblem.js
+++ leetcodeJS/problems/mediumProblem.js
@@ -23,35 +23,39 @@
 // console.log(twoSum(numbers, target)); // [1,2
 
 
-// 15. 3Sum
-var threeSum = function(nums) {
-  const sortedNums =  nums.sort((a,b) => a -b);
-  const result = [[]];
-  let firstPointer = 0;
+// // 15. 3Sum  // incomplete // completed
+// var threeSum = function(nums) {
+//   const sortedNums =  nums.sort((a,b) => a -b);
+//   const result = [];
+//   let firstPointer = 0;
 
-  let sum = 0;
-  for( firstPointer = 0; firstPointer < sortedNums.length -2; firstPointer++) {
-    let leftPointer = firstPointer + 1;
-    let rightPointer = sortedNums.length -1;
-    while(leftPointer < rightPointer) {
-      sum = sortedNums[firstPointer] + sortedNums[leftPointer] + sortedNums[rightPointer];
-      if(sum === 0)
-        {
-          result.push([sortedNums[firstPointer], sortedNums[leftPointer], sortedNums[rightPointer]])
-        }
-        else if(sum < 0)
-        {
-          leftPointer++;
-        }
-        else
-        {
-          rightPointer--;
-        }
-    }
-  }
-  return result;
+//   let sum = 0;
+//   for( firstPointer = 0; firstPointer < sortedNums.length -2; firstPointer++) {
+//     let leftPointer = firstPointer + 1;
+//     let rightPointer = sortedNums.length -1;
+//     while(leftPointer < rightPointer) {
+//       sum = sortedNums[firstPointer] + sortedNums[leftPointer] + sortedNums[rightPointer];
+//       if(sum === 0)
+//         {
+//           result.push([sortedNums[firstPointer], sortedNums[leftPointer], sortedNums[rightPointer]])
+//           leftPointer++;
+//           rightPointer--;
+//         }
+//         else if(sum < 0)
+//         {
+//           leftPointer++;
+//         }
+//         else
+//         {
+//           rightPointer--;
+//         }
+//     }
+//   }
+//   let uniqueResult = Array.from(new Set(result.map(JSON.stringify))).map(JSON.parse);
+//   // console.log(uniqueResult);
+//   return uniqueResult;
     
-};
+// };
 
-const nums = [-1,0,1,2,-1,-4];
-console.log(threeSum(nums)); // [[-1,-1,2],[-1,0,1]]
+// const nums = [0,0,0];
+// console.log(threeSum(nums)); // [[-1,-1,2],[-1,0,1]]
