b94af9b4e29d387952369c12201c8cfa601c34c2
amanullahshah32 <nafi17211@gmail.com>
2026-02-05 11:45:20 +0600

    constructor functions working strategy leaned

--- 14_OOP/script.js
+++ 14_OOP/script.js
@@ -1,1 +1,19 @@
-'use strict';
+"use strict";
+
+const Person = function (firstName, birthYear) {
+//    console.log(this);
+   this.firstName = firstName; 
+    this.birthYear = birthYear;
+};
+
+const aman = new Person('Aman', 2001); 
+console.log(aman);
+
+const noshin = new Person('Noshin', 2000);
+console.log(noshin);
+
+
+// 1. a new empty object {} is created
+// 2. function is called, this = nwe emplty object {}
+// 3. {} linked to prototype
+// 4. function automatically return empty object {}
