4336b712707c3e98721dad70e100e29382804ef2
amanullahshah32 <nafi17211@gmail.com>
2026-02-05 11:52:35 +0600

    shouldnt have created method inside constructor function

--- 14_OOP/script.js
+++ 14_OOP/script.js
@@ -2,8 +2,14 @@
 
 const Person = function (firstName, birthYear) {
 //    console.log(this);
+// Instance properties
    this.firstName = firstName; 
     this.birthYear = birthYear;
+
+    // // adding methods
+    // this.calcAge = function(){
+    //     console.log( 2026 - this.birthYear);
+    // }
 };
 
 const aman = new Person('Aman', 2001); 
@@ -17,3 +23,6 @@ console.log(noshin);
 // 2. function is called, this = nwe emplty object {}
 // 3. {} linked to prototype
 // 4. function automatically return empty object {}
+
+
+console.log(aman instanceof Person);
