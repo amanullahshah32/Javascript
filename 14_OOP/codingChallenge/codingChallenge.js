4ebcc2ea39430b2d0a79be6d280898cf59c960f3
amanullahshah32 <nafi17211@gmail.com>
2026-02-09 14:26:16 +0600

    coding challege 2 done

--- 14_OOP/codingChallenge/codingChallenge.js
+++ 14_OOP/codingChallenge/codingChallenge.js
@@ -1,25 +1,23 @@
-'use strict';
+"use strict";
 
 console.log(`Coding challenge 1:`);
 
-const Car = function(make, speed)
-{
-    this.make = make;
-    this.curSpeed = speed;
+const Car = function (make, speed) {
+  this.make = make;
+  this.curSpeed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.curSpeed += 10;
+  console.log(`${this.make} is going at ${this.curSpeed} km/h`);
+};
 
-}
-Car.prototype.accelerate = function(){
-    this.curSpeed +=10;
-    console.log(`${this.make} is going at ${this.curSpeed} km/h`);
-}
+Car.prototype.brake = function () {
+  this.curSpeed -= 5;
+  console.log(`${this.make} is going at ${this.curSpeed} km/h`);
+};
 
-Car.prototype.brake = function(){
-    this.curSpeed -=5;
-    console.log(`${this.make} is going at ${this.curSpeed} km/h`);
-}
-
-const toyota = new Car('TOYOTA', 100);
-const bmw = new Car('BMW', 120);
+const toyota = new Car("TOYOTA", 100);
+const bmw = new Car("BMW", 120);
 console.log(toyota);
 console.log(bmw);
 toyota.accelerate();
@@ -27,3 +25,42 @@ toyota.accelerate();
 toyota.accelerate();
 toyota.brake();
 console.log(toyota);
+
+console.log(`coding challenge 2: \n`);
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.curSpeed = speed;
+  }
+
+  get speedUS() {
+    return this.curSpeed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.curSpeed = speed * 1.6;
+  }
+
+  accelerate() {
+    this.curSpeed += 10;
+    console.log(`${this.make} is going at ${this.curSpeed} km/h`);
+  }
+  brake() {
+    this.curSpeed -= 5;
+    console.log(`${this.make} is going at ${this.curSpeed} km/h`);
+  }
+}
+
+const ford = new CarCl("Ford", 120);
+console.log(ford);
+ford.accelerate();
+ford.accelerate();
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+
+console.log(ford);
+console.log(ford.speedUS);
+ford.speedUS = 199;
+console.log(ford);
