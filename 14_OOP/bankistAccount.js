d9c761161784ccc4c09d726a42e6b2eb98b9dee1
amanullahshah32 <nafi17211@gmail.com>
2026-02-17 22:15:09 +0600

    private  methods learned

--- 14_OOP/bankistAccount.js
+++ 14_OOP/bankistAccount.js
@@ -5,6 +5,8 @@
 // Public methods
 // Private methods
 
+// (there is also the static version of all of these)
+
 
 class Account {
     // 1) public instance fields // public fileds (instanced properties)
@@ -12,16 +14,20 @@ class Account {
     
     // 2) Private instance fields (instanced properties)
     #movements = [];
+    #pin; 
     constructor(owner, currency, pin){
         this.owner = owner;
         this.currency = currency;
-        this._pin = pin;
+        this.#pin = pin;
         // protected property: _movements
         // this._movements = [];
         // this.locale = navigator.language;
 
         console.log(`Thanks for opening an account, ${owner}`);
     }
+    // 3) Public methods
+
+
     // Public interface of our object
 
     getMovements(){
@@ -33,15 +39,22 @@ class Account {
     withdraw(val){
         this.deposit(-val);
     }
-    _approveLoan(val){
-        return true;
-    }
+
     requestLoan(val){
-        if(this._approveLoan(val)){
+        if(this.#approveLoan(val)){
             this.deposit(val);
             console.log(`loan appoved of BDT ${val} TAKA`);
         }
     }
+    static helper(){
+        console.log('Helper is a static method');
+    }
+
+    // 4) private methods
+        #approveLoan(val){
+        return true;
+    }
+    
 }
 
 const acc1 = new Account('Aman', 'BDT', 1234);
@@ -56,6 +69,11 @@ console.log(acc1.getMovements());
 console.log(acc1._pin);
 acc1.requestLoan(1250);
 console.log(acc1.getMovements());
-acc1._approveLoan(1260);
+// acc1.#approveLoan(1260);
+acc1.requestLoan(1260);
+console.log(acc1.getMovements());
 
-console.log(acc1.#movements);
+// console.log(acc1.#pin); // Private fields are not accessible outside the class body
+// console.log(acc1.#movements); // Private fields are not accessible outside the class body
+
+Account.helper(); // only workd on the class not in the instances
