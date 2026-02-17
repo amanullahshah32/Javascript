1ed4abac9a342b67fdcc22fa36fa0225ab917aee
amanullahshah32 <nafi17211@gmail.com>
2026-02-17 21:26:18 +0600

    protected methods

--- 14_OOP/bankistAccount.js
+++ 14_OOP/bankistAccount.js
@@ -4,24 +4,29 @@ class Account {
     constructor(owner, currency, pin){
         this.owner = owner;
         this.currency = currency;
-        this.pin = pin;
-        this.movements = [];
+        this._pin = pin;
+        // protected property: _movements
+        this._movements = [];
         this.locale = navigator.language;
 
         console.log(`Thanks for opening an account, ${owner}`);
     }
     // Public interface of our object
+
+    getMovements(){
+        return this._movements;
+    }
     deposit(val){
-        this.movements.push(val);
+        this._movements.push(val);
     }
     withdraw(val){
         this.deposit(-val);
     }
-    approveLoan(val){
+    _approveLoan(val){
         return true;
     }
     requestLoan(val){
-        if(this.approveLoan(val)){
+        if(this._approveLoan(val)){
             this.deposit(val);
             console.log(`loan appoved of BDT ${val} TAKA`);
         }
@@ -31,13 +36,13 @@ class Account {
 const acc1 = new Account('Aman', 'BDT', 1234);
 console.log(acc1);
 
-// acc1.movements.push(250);
+// acc1._movements.push(250);
 // acc1.movements.push(-140);
 
 acc1.deposit(250);
 acc1.withdraw(140);
-console.log(acc1.movements);
-console.log(acc1.pin);
+console.log(acc1.getMovements());
+console.log(acc1._pin);
 acc1.requestLoan(1250);
-console.log(acc1.movements);
-acc1.approveLoan(1260);
+console.log(acc1._movements);
+acc1._approveLoan(1260);
